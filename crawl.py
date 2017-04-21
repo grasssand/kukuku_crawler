12e7325855c62374c2c5d937dc7de0d8fde5945a
GrassSand <grasssand@gmail.com>
2017-04-21 14:08:56 +0800

    断点续爬

--- crawl.py
+++ crawl.py
@@ -15,13 +15,11 @@ import aiohttp
 import orm
 from models import Thread, Reply
 
-from test_logging import log_info
-
 THREADS_URL = 'http://h.koukuko.com/api/{}?page={}'
 REPLYS_URL = 'http://h.koukuko.com/api/t/{}?page={}'
 IMAGE_URL = 'http://static.kukuku.cc/{}'
-FORUM_TYPE = ['综合版1']
-MAX_PAGES = None
+FORUM_TYPE = ['综合版1', ]
+MAX_PAGES = 1000
 IMAGE_FOLDER = os.path.join(sys.path[0], 'static')
 DB_SETTING = {
     'user': 'postgres',
@@ -37,6 +35,9 @@ if not os.path.exists(IMAGE_FOLDER):
 def is_redirect(response):
     return response.status in (300, 301, 302, 303, 307)
 
+def timestamp2datetime(ts):
+    return datetime.fromtimestamp(ts / 1000)
+
 
 class Crawler:
 
@@ -49,6 +50,9 @@ class Crawler:
         self.q = Queue(loop=self.loop)
         self.seen_urls = set()
         self.session = aiohttp.ClientSession(loop=self.loop)
+        self.last_created = None
+        self.last_updated = None
+        self._stopped = False
         for t in FORUM_TYPE:
             self.add_url(THREADS_URL.format(t, 1))
         self.t0 = time.time()
@@ -68,9 +72,9 @@ class Crawler:
                     f.write(content)
         return img_name
 
-    async def save_db(self, item):
-        item['created_at'] = datetime.fromtimestamp(item['createdAt'] / 1000)
-        item['updated_at'] = datetime.fromtimestamp(item['updatedAt'] / 1000)
+    async def save_data(self, item):
+        item['created_at'] = timestamp2datetime(item['createdAt'])
+        item['updated_at'] = timestamp2datetime(item['updatedAt'])
         if item.get('parent'):
             r = Reply(**item)
         else:
@@ -78,23 +82,48 @@ class Crawler:
         try:
             await r.save()
         except Exception as e:
-            log_info.error(item)
-            log_info.error('save database error: %r', e)
+            LOGGER.error('save database error: %r', e)
+
+    async def update_data(self, item):
+        item['created_at'] = timestamp2datetime(item['createdAt'])
+        item['updated_at'] = timestamp2datetime(item['updatedAt'])
+        r = Thread(**item)
+        await r.update()
 
     async def parse_thread_link(self, response):
+        '''抓取串'''
         links = set()
         next_url = None
         body = await response.json()
-
         if response.status == 200:
             for thread in body['data']['threads']:
+                # 排除置顶串
+                thread_id = thread['id']
+                if thread_id == 6960723:
+                    continue
+                # 断点续爬
+                updated_at = timestamp2datetime(thread['updatedAt'])
+                if (self.last_updated and 
+                        self.last_updated.getValue('updated_at') >= updated_at):
+                    self._stopped = True
+                    break
+
+                # 串内回复url
                 pages = (thread['replyCount'] + 19) // 20
                 urls = [REPLYS_URL.format(thread['id'], i)
                         for i in range(pages, 0, -1)]
                 if urls:
-                    LOGGER.info('got %r urls from %r', len(urls), response.url)
+                    LOGGER.info('got %r urls from %r', 
+                                len(urls), response.url)
                 links.update(urls)
-                await self.save_db(thread)
+
+                # 判断新串
+                created_at = timestamp2datetime(thread['createdAt'])
+                if (self.last_created is None or
+                        self.last_created.getValue('created_at') < created_at):
+                    await self.save_data(thread)
+                else:
+                    await self.update_data(thread)
 
             size = body['page']['size']
             location = body['page']['page']
@@ -106,11 +135,15 @@ class Crawler:
         return links, next_url
 
     async def parse_reply_link(self, response):
+        '''抓取回复'''
         body = await response.json()
 
         if response.status == 200:
             for reply in body['replys']:
-                await self.save_db(reply)
+                created_at = timestamp2datetime(reply['createdAt'])
+                if (self.last_updated is None or
+                        self.last_updated.getValue('updated_at') < created_at):
+                    await self.save_data(reply)
 
     async def fetch(self, url, max_redirect):
         print('fetch url: %s' % url)
@@ -121,7 +154,8 @@ class Crawler:
                 response = await self.session.get(url, allow_redirects=False)
                 break
             except aiohttp.ClientError as client_error:
-                LOGGER.info('try %r for %r raised %r', tries, url, client_error)
+                LOGGER.info('try %r for %r raised %r', 
+                            tries, url, client_error)
                 exception = client_error
             tries += 1
         else:
@@ -161,18 +195,26 @@ class Crawler:
                 assert url in self.seen_urls
                 await self.fetch(url, max_redirect)
                 self.q.task_done()
-                asyncio.sleep(1)
+                asyncio.sleep(2)
         except asyncio.CancelledError:
             pass
 
     def add_url(self, url, max_redirect=None):
         if max_redirect is None:
             max_redirect = self.max_redirect
-        self.seen_urls.add(url)
-        self.q.put_nowait((url, max_redirect))
+        if not self._stopped:
+            self.seen_urls.add(url)
+            self.q.put_nowait((url, max_redirect))
 
     async def crawl(self):
         await orm.create_pool(loop=self.loop, **DB_SETTING)
+        # 最后一次回复的串
+        updated = await Thread.findAll(orderBy='updated_at desc', limit=1)
+        self.last_updated = updated[0] if updated else None
+        # 最后一次创建的串
+        created = await Thread.findAll(orderBy='created_at desc', limit=1)
+        self.last_created = created[0] if created else None
+
         workers = [asyncio.Task(self.work(), loop=self.loop)
                    for _ in range(self.max_tasks)]
         self.t0 = time.time()
@@ -180,7 +222,7 @@ class Crawler:
         self.t1 = time.time()
         for w in workers:
             w.cancel()
-        # await orm.close_pool()
+        await orm.close_pool()
 
 
 if __name__ == '__main__':
@@ -190,5 +232,4 @@ if __name__ == '__main__':
     print('Finished {} urls in {:.3f} secs'.format(
           len(crawler.seen_urls), crawler.t1 - crawler.t0))
     crawler.close()
-
     loop.close()
